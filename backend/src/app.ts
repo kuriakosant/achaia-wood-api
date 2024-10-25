ce9f7b980f199f053f6515fa9d0b25e5f9b9d2cb
kuriakosant <kuriakosant2003@gmail.com>
2024-10-25 12:00:00 +0300

    create product models

---
+++ backend/src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import productRoutes from './routes/productRoutes';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI || '', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.log(error));
+
+// Routes
+app.use('/api', productRoutes);
+
+export default app;
