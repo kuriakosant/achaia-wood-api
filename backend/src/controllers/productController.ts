bdf672b581deba68099559c5443bd4127414541c
kuriakosant <kuriakosant2003@gmail.com>
2024-10-25 12:00:00 +0300

    create app.ts server.ts controllers

---
+++ backend/src/controllers/productController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import { Product } from '../models/productModel';
+
+export const getProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const createProduct = async (req: Request, res: Response) => {
+  try {
+    const newProduct = new Product(req.body);
+    const savedProduct = await newProduct.save();
+    res.status(201).json(savedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
