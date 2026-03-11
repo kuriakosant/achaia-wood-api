1f5d852207f22c250d4008075f41fbff6825db19
kuriakosant <kuriakosant2003@gmail.com>
2026-03-11 14:04:44 +0200

    fix(deploy): use DATABASE_URL string inside sequelize for Vercel Serverless

--- backend/src/sequelize.ts
+++ backend/src/sequelize.ts
@@ -3,13 +3,25 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize({
-  dialect: 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT as string, 10),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-});
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    logging: false, // Prevent terminal spam
+  })
+  : new Sequelize({
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '5432', 10),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    logging: false,
+  });
 
 export default sequelize;
